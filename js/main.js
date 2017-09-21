e34aafadd4cb00896e015ff88913e87d34613676
Зюзько Олег Михайлович <Zyuzko.OM@gazprom-neft.local>
2017-09-21 17:05:53 +0600

    изменена галерея добавлено маштабирование галереи

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
-var name="dress";
+/*function vis(){
+	var a=document.getElementById('qq');
+	console.log(a.classList);
+	a.classList.remove('productItem-none'); 
+}*/
 
-function dressClick(){
-	var a=document.getElementById("Dress");
-	console.log(a);
+function vis(){
+	var elements = document.querySelectorAll('.productItem-none');
+
+	for (var i = 0; i < 4; i++) 
+	{
+		elements[i].classList.remove('productItem-none');
+	}
+
+	var elements = document.querySelectorAll('.productItem-none');
+		
+	if (elements.length == 0)
+	{
+		var button = document.getElementById("butt");
+		button.disabled = true;
+	}
 }
