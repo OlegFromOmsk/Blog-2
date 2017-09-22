636c7eacda69cd7df8293b62e94765cfd5a35c2b
OlegFromOmsk <32164733+OlegFromOmsk@users.noreply.github.com>
2017-09-23 00:20:37 +0600

    add change-foto
    добавлено обновление галереи картинок по клику на меню

--- js/main.js
+++ js/main.js
@@ -14,13 +14,46 @@ function vis(){
 	{
 		var button = document.getElementById("butt");
 		button.disabled = true;
+        button.innerHTML='Конец';
 	}
 }
 
 
-$(function () {
-    var location = window.location.href;
-    var cur_url = '/' + location.split('/').pop();
- 
-    console.log(cur_url);
-});
+/*$(document).ready(function(){
+    $(".header_nav a").click(function() {
+		console.log($('.header_nav li a').attr(alt));
+		$('.header_nav li').removeClass('active');
+		$(this).parent('li').addClass('active');
+    });
+});*/
+
+/*$(document).ready(function(){
+    $("nav a").click(function(){
+        var Alt = $(this).attr("alt");
+        console.log(Alt);
+    var aaa;
+  $('.all_work a').each(function (index, value) {
+  			console.log('Порядковый номер: ' + (index+1)+ ' ' +value);
+/*		$(".all_work a").attr({ href: "img/" + Alt + "/p_" + (index + 1) + ".PNG"});
+			$(".all_work img").attr({ src: "img/" + Alt + "/p_" + (index + 1) + "_s.PNG", alt: Alt});*/
+ /*     return false;
+		});
+    });
+ });*/
+
+$(document).ready(function(){
+    $("nav a").click(function(){
+        var Alt = $(this).attr("alt");
+        console.log(Alt);
+    var aaa;
+  $('.all_work').find('a').each(function (index) {
+  			console.log('Порядковый номер: ' + (index+1));
+      console.log($(this).parent('a'));
+$(this).attr({ href: "img/" + Alt + "/p_" + (index + 1) + ".jpg"});
+$(this).find('img').attr({ src: "img/" + Alt + "/p_" + (index + 1) + "_s.jpg", alt: Alt});
+
+		});
+    });
+ });
+
+
