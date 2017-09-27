1fbb861a5bff20b7738c0d129be90e8aa572c442
Зюзько Олег Михайлович <Zyuzko.OM@gazprom-neft.local>
2017-09-27 09:13:38 +0600

    12

--- js/main.js
+++ js/main.js
@@ -1,22 +1,19 @@
 function vis(){
-	var elements = document.querySelectorAll('.productItem-none');
+	var elements = document.querySelectorAll('.none');
 	for (var i = 0; i < 4; i++) 
 	{
-		elements[i].classList.remove('productItem-none');
+		elements[i].classList.remove('none');
 	}
-	var elements = document.querySelectorAll('.productItem-none');
+	var elements = document.querySelectorAll('.none');
     if (elements.length == 0)
 	{
-		var button = document.getElementById("butt");
-		button.disabled = true;
-        button.innerHTML='Конец';
+		document.getElementById("butt").classList.add('none');
 	}
 	
 	window.scrollTo(0, document.body.scrollHeight);
 }
 
 var arr = new Array();
-
 function ReadText(names){
           $.ajax({
             url: "img/" + names + "/descr.txt",
@@ -24,7 +21,10 @@ function ReadText(names){
             async: true,
             success: function(msg){
             arr=msg.trim().match(/[^\r\n]+/g);
-            console.log(arr[8]);    
+            
+				$('.all_work').find('.productItem').each(function (index) {
+					$(this).find('.highslide-caption').text(arr[index]);   
+        		});
             }
         });  
 }
@@ -32,33 +32,28 @@ function ReadText(names){
 $(document).ready(function(){
     $("nav a").click(function(){
         var Alt = $(this).attr("alt");
-console.log('1');
-          $.ajax({
-            url: "img/" + Alt + "/descr.txt",
-            dataType: "text",
-            /*async: true,*/
-            success: function(msg){
-            arr=msg.trim().match(/[^\r\n]+/g);
-            console.log(arr[8]);    
-            }
-        });  
- console.log('2');       
+		/*$(this).classList.add('active');*/
+
+ console.log('1');       
         var elements = document.querySelectorAll('.all_work .productItem');
         for (var q = 4; q < 12; q++)
         {
-            elements[q].classList.add('productItem-none');
+            elements[q].classList.add('none');
         }
-console.log('3');
+		document.getElementById("butt").classList.remove('none');
+console.log('2');
         var button = document.getElementById("butt");
         button.disabled = false;
         button.innerHTML='Посмотреть еще...';
-console.log('4');
+console.log('3');
        $('.all_work').find('.productItem').each(function (index) {
             $(this).find('a').attr({ href: "img/" + Alt + "/p_" + (index + 1) + ".jpg"});
             $(this).find('img').attr({ src: "img/" + Alt + "/p_" + (index + 1) + "_s.jpg", alt: Alt});
-            console.log(arr[index]);
+console.log(arr[index]);
             $(this).find('.highslide-caption').text(arr[index]);   
         });
  console.log('5');
+		ReadText(Alt);	
+console.log('5');
     });
  });
